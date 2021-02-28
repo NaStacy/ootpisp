1d8834102ee26d39db7c3b4c1f63e91348f7495b
meow <nasty0701@gmail.com>
2021-03-01 02:26:44 +0300

    load first lab

---
+++ oop1/oop1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace oop1
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(38, 299);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(229, 59);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "generate figure";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 280);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(304, 370);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn);
+            this.Name = "Form";
+            this.Text = "geometric figures";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ oop1/oop1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black, 3);
+            Random random = new Random();
+            int a;
+            a = random.Next(5);
+            if (a == 1)
+            {
+                pictureBox1.Image = null;
+                graph.DrawLine(pen, 10, 10, 258, 258);
+                pictureBox1.Image = bmp;
+            }
+            if (a == 2)
+            {
+                pictureBox1.Image = null;
+                graph.DrawRectangle(pen, 10, 10, 258, 258);
+                pictureBox1.Image = bmp;
+            }
+            if (a == 3)
+            {
+                pictureBox1.Image = null;
+                graph.DrawEllipse (pen, 10, 10, 258, 258);
+                pictureBox1.Image = bmp;
+            }
+            if (a == 4)
+            {
+                pictureBox1.Image = null;
+                graph.DrawLine(pen, 130, 10, 258, 258);
+                graph.DrawLine(pen, 258, 258, 10, 258);
+                graph.DrawLine(pen, 10, 258, 130, 10);
+                pictureBox1.Image = bmp;
+            }
+            if (a == 0)
+            {
+                pictureBox1.Image = null;
+                graph.DrawLine(pen, 10, 10, 50, 258);
+                graph.DrawLine(pen, 50, 258, 110, 10);
+                graph.DrawLine(pen, 110, 10, 175, 258);
+                graph.DrawLine(pen, 175, 258, 258, 10);
+                pictureBox1.Image = bmp;
+            } 
+        }
+    }
+}
